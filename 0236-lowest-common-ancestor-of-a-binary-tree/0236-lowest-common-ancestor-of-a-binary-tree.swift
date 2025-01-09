f27eca22f429aa013cc9523af8e5a8ebce73abb9
KoustubhSahu <64017883+KoustubhSahu@users.noreply.github.com>
2025-01-08 22:33:37 -0500

    Time: 24 ms (98.06%), Space: 17.5 MB (75.79%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.swift
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
+        if root == nil || root === p || root === q {
+            return root
+        }
+
+        let left = lowestCommonAncestor(root?.left, p, q)
+        let right = lowestCommonAncestor(root?.right, p, q)
+
+        if left != nil && right != nil {
+            return root
+        }
+
+        return left ?? right
+    }
+}
