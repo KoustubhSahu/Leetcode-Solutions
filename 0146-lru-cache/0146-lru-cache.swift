b6cf28be836463a85e57c3f6720e0b25ddb9a009
KoustubhSahu <64017883+KoustubhSahu@users.noreply.github.com>
2025-01-08 20:23:17 -0500

    Time: 160 ms (22.08%), Space: 33.6 MB (65.31%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.swift
@@ -0,0 +1,81 @@
+
+class LRUCache {
+    class Node {
+        var key: Int
+        var val: Int
+        var prev: Node? = nil
+        var next: Node? = nil
+
+        init(_ key: Int, _ val: Int) {
+            self.key = key
+            self.val = val
+        }
+    }
+
+    var head: Node? = nil
+    var tail: Node? = nil
+    var capacity: Int
+    var map = [Int: Node?]()
+
+    init(_ capacity: Int) {
+        self.capacity = capacity
+    }
+    
+    func get(_ key: Int) -> Int {
+        if let node = map[key] {
+            delete(node)
+            add(node)
+            return node!.val
+        }
+
+        return -1
+    }
+    
+    func put(_ key: Int, _ value: Int) {
+        let newNode: Node? = Node(key, value)
+        if let node = map[key] {
+            delete(node)
+        } else if map.count == capacity {
+            map.removeValue(forKey: head!.key)
+            delete(head)
+        }
+        add(newNode)
+        map[key] = newNode
+    }
+
+    func add(_ node: Node?) {
+        if head == nil && tail == nil {
+            head = node
+            tail = node
+        } else {
+            node?.prev = tail
+            tail?.next = node
+            tail = tail?.next
+        }
+    }
+
+    func delete(_ node: Node?) {
+        if node?.prev == nil && node?.next == nil {
+            head = nil
+            tail = nil
+        } else if node?.prev == nil {
+            head = head?.next
+            head?.prev = nil
+        } else if node?.next == nil {
+            tail = tail?.prev
+            tail?.next = nil
+        } else {
+            node?.prev?.next = node?.next
+            node?.next?.prev = node?.prev
+            node?.next = nil
+            node?.prev = nil
+        }
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * let obj = LRUCache(capacity)
+ * let ret_1: Int = obj.get(key)
+ * obj.put(key, value)
+ */
