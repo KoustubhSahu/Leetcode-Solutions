9b7be609d6f04215203b0dc233a703ef53073930
KoustubhSahu <64017883+KoustubhSahu@users.noreply.github.com>
2025-01-09 22:30:31 -0500

    Time: 31 ms (90.45%), Space: 22.6 MB (50.75%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.swift
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.swift
@@ -1,11 +1,10 @@
 class Solution {
     func calculate(_ s: String) -> Int {
-        let sArr = Array(s + "+")
         var stack = [Int]()
         var currentNumber = 0
         var lastOperation: Character = "+"
-        // let temp = s + "+"
-        for ch in sArr {
+        let temp = s + "+"
+        for ch in temp {
             if ch.isNumber {
                 currentNumber = currentNumber * 10 + Int(String(ch))!
             } else {
