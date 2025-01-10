50bbd4568638c0f5fe6dac46228e5ffafd75c7cf
KoustubhSahu <64017883+KoustubhSahu@users.noreply.github.com>
2025-01-09 23:39:13 -0500

    Time: 20 ms (41.28%), Space: 19.2 MB (9.62%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func subarraySum(_ nums: [Int], _ k: Int) -> Int {
+        var map = [0: 1]
+        var sum = 0, count = 0
+
+        for num in nums {
+            sum += num
+
+            if let value = map[sum - k] {
+                count += value
+            }
+            map[sum, default: 0] += 1
+        }
+
+        return count
+    }
+}
