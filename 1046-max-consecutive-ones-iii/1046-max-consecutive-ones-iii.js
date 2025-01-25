99fad3d3bc6fdf9a00dec771da73fcc59bda3977
KoustubhSahu <64017883+KoustubhSahu@users.noreply.github.com>
2025-01-24 20:04:05 -0500

    Time: 1 ms (98.16%), Space: 54.6 MB (57.54%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.js
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.js
@@ -5,22 +5,22 @@
  */
 var longestOnes = function(nums, k) {
     let [i, j] = [0, 0];
-    let [queue, front] = [[], 0];
     let max = 0;
-
+    let zero = 0;
     while (j < nums.length) {
         if (nums[j] == 0) {
-            queue.push(j);
-            if (k==0) {
-                i = queue[front] + 1;
-                front++;
-            } else {
-                k--;
-            }
+            zero++;
         }
+
+        while (zero>k) {
+            if (nums[i] == 0) {
+                zero--;
+            }
+            i++;
+        }
+
         let temp = j-i+1;
         max = max>temp ? max : temp;
-        // max = Math.max(max, j-i+1);
         j++;
     }
 
