7c9b7ec1c1cc2091c97d4680b3cb1383f84f2a4e
KoustubhSahu <64017883+KoustubhSahu@users.noreply.github.com>
2025-01-24 19:16:05 -0500

    Time: 0 ms (100%), Space: 50.1 MB (85.3%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function(root) {
+    let sum = 0;
+    let pathNum = 0;
+
+    function dfs (currNode, currPathNum) {
+        if (currNode == null) return -1;
+
+        currPathNum = currPathNum*10 + currNode.val;
+        let left = dfs(currNode.left, currPathNum);
+        let right = dfs(currNode.right, currPathNum);
+
+        if (left==-1 && right==-1) {
+            sum += currPathNum;
+        }
+
+        return currPathNum;
+    }
+
+    dfs(root, pathNum);
+
+    return sum;
+};
