0689dd1a3efaae603722dd6a87e2d3cfe1071956
KoustubhSahu <64017883+KoustubhSahu@users.noreply.github.com>
2025-01-25 20:38:04 -0500

    Time: 7 ms (22.51%), Space: 50.8 MB (77.81%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    let carry = 0;
+    let [i, j] = [num1.length-1, num2.length-1];
+    let result = [];
+    while (i>=0 || j>=0) {
+        let currSum = carry;
+        if (i>=0) {
+            currSum += parseInt(num1[i])
+        }
+        if (j>=0) {
+            currSum += parseInt(num2[j])
+        }
+
+        carry = parseInt(currSum/10);
+        result.push(`${currSum%10}`);
+        i--;
+        j--;
+    }
+
+    if (carry == 1) result.push("1");
+
+    return result.reverse().join("");
+};
