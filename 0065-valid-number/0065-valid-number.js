449761f0ac4af9b2d48d0646a6455dcc84fd97a7
KoustubhSahu <64017883+KoustubhSahu@users.noreply.github.com>
2025-01-28 22:13:13 -0500

    Time: 0 ms (100%), Space: 52.9 MB (88.53%) - LeetHub

--- 0065-valid-number/0065-valid-number.js
+++ 0065-valid-number/0065-valid-number.js
@@ -2,47 +2,8 @@
  * @param {string} s
  * @return {boolean}
  */
-var isNumber = function(s) {
+let isNumber = function(s) {
     if(s == 'Infinity' || s == '-Infinity' || s == '+Infinity')
         return false;
     return !isNaN(s);
-};
-
-
-var isNumber2 = function(s) {
-    if (s == "") return false;
-    s = s.toLowerCase();
-    if (s[0] == "e" || s[s.length-1] == "e") return false;
-    let arr = s.split("e");
-    if (arr.length>2) return false;
-
-    if (!isValid(arr[0], true)) {
-        return false
-    }
-
-    if (arr.length == 2) {
-        return isValid(arr[1], false)
-    }
-
-    return true
-
-    function isValid(str, allowDecimal) {
-
-        let hasNumber = false
-        let hasDecimal = false
-        for (let i=0; i<str.length; i++) {
-            let ch = str[i]
-            if (ch == "+" || ch == "-") {
-                if (i != 0) { return false } 
-            } else if (ch == ".") {
-                if (!allowDecimal || hasDecimal) { return false }
-                hasDecimal = true
-            } else if (isNaN(Number(ch))) {
-                return false
-            } else {
-                hasNumber = true
-            }
-        }
-        return hasNumber
-    }
 };
