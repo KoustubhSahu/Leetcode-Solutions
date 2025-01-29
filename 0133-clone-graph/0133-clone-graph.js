0ba28cea7663e9be39a79c46c5121b100a46b80e
KoustubhSahu <64017883+KoustubhSahu@users.noreply.github.com>
2025-01-28 19:43:39 -0500

    Time: 67 ms (22.55%), Space: 51.9 MB (50.84%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.js
+++ 0133-clone-graph/0133-clone-graph.js
@@ -14,11 +14,14 @@ var cloneGraph = function(node) {
     if (!node) return null;
     let visitedNode = new Map();
     let nodeCopy = new Node(node.val);
+    let visited = new Set();
     visitedNode.set(node, nodeCopy);
-    cloneNode(node, nodeCopy);
+
+    bfs(node, nodeCopy);
     return nodeCopy;
 
-    function cloneNode(currNode, copyNode) {
+
+    function bfs(currNode, copyNode) {
         for (n of currNode.neighbors) {
             if (visitedNode.has(n)) {
                 copyNode.neighbors.push(visitedNode.get(n));
@@ -26,7 +29,26 @@ var cloneGraph = function(node) {
                 let newNode = new Node(n.val);
                 visitedNode.set(n, newNode);
                 copyNode.neighbors.push(newNode);
-                cloneNode(n, newNode);
+            }
+        }
+        visited.add(currNode);
+        console.log(`${copyNode.val} -> ${copyNode.neighbors.map((a) => a.val)}`);
+        for (n of currNode.neighbors) {
+            if (!visited.has(n)) {
+                bfs(n, visitedNode.get(n));
+            }
+        }
+    }
+
+    function dfs(currNode, copyNode) {
+        for (n of currNode.neighbors) {
+            if (visitedNode.has(n)) {
+                copyNode.neighbors.push(visitedNode.get(n));
+            } else {
+                let newNode = new Node(n.val);
+                visitedNode.set(n, newNode);
+                copyNode.neighbors.push(newNode);
+                dfs(n, newNode);
             }
         }
     }
