332da5d77e431f1cbe83b997ba14cd228e08452f
KoustubhSahu <64017883+KoustubhSahu@users.noreply.github.com>
2025-02-08 19:08:52 -0500

    Time: 34 ms (32.77%), Space: 53 MB (64.2%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.js
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.js
@@ -3,6 +3,17 @@
  * @param {number} k
  * @return {boolean}
  */
+// var canConstruct = function(s, k) {
+//     if (k > s.length) return false;
+//     let oddSet = new Set();
+//     for (let ch of s) {
+//         if (oddSet.has(ch)) oddSet.delete(ch);
+//         else oddSet.add(ch);
+//     }
+
+//     return oddSet.size<=k;
+// };
+
 var canConstruct = function(s, k) {
     if (k > s.length) return false;
     let map = {};
